59b8ada74de5609128e79a494cf2c89e3718362c
Barbara Brigolin <barbarabrigolin@hotmail.com>
2023-01-03 19:48:45 -0300

    organizei os grupos e importei alamofire

---
+++ AppDesafioChuckNorris/AppDesafioChuckNorris/Sources/Categories/Screen/CategoriesScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesScreen.swift
+//  AppDesafioChuckNorris
+//
+//  Created by Barbara Brigolin on 03/01/23.
+//
+
+import UIKit
+
+class CategoriesScreen: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ AppDesafioChuckNorris/AppDesafioChuckNorris/Sources/Categories/ViewModel/CategoriesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoriesViewModel.swift
+//  AppDesafioChuckNorris
+//
+//  Created by Barbara Brigolin on 03/01/23.
+//
+
+import UIKit
+
+class CategoriesViewModel {
+
+}
